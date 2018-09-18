a51d994ad07e264409fdc915687c21d0f512dc52
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2018-09-17 20:34:08 -0400

    Update tweepy.py

--- tweepy.py
+++ tweepy.py
@@ -2,6 +2,7 @@ import tweepy
 from tweepy import OAuthHandler
 import json
 import os
+import urllib.request
 
 consumer_key = ''
 consumer_secret = ''
@@ -14,7 +15,7 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 tweets = api.user_timeline(screen_name='KrisWu',
-                           count=200, include_rts=False,
+                           count=10, include_rts=False,
                            exclude_replies=True)
 media_files = set()
 for status in tweets:
@@ -22,9 +23,10 @@ for status in tweets:
     if(len(media) > 0):
         media_files.add(media[0]['media_url'])
 
-import wget
-
+num=0
 for media_file in media_files:
-        wget.download(media_file)
+    save_name = 'img%03d.jpg'%num
+    urllib.request.urlretrieve(media_file,save_name)
+    num = num + 1
 
 os.popen('ffmpeg -r 0.5 -i img%03d.jpg -vf scale=500:500 -y -r 30 -t 60 out.mp4')

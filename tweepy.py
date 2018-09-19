77bd6d13e493e2ecfd43654c00b9946e94482224
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2018-09-19 15:49:11 -0400

    Update tweepy.py

--- tweepy.py
+++ tweepy.py
@@ -2,7 +2,12 @@ import tweepy
 from tweepy import OAuthHandler
 import json
 import os
+import io
 import urllib.request
+from google.cloud import vision
+from google.cloudself.vision import types
+
+from PIL import Image,ImageFont,ImageDraw
 
 consumer_key = ''
 consumer_secret = ''
@@ -28,5 +33,35 @@ for media_file in media_files:
     save_name = 'img%03d.jpg'%num
     urllib.request.urlretrieve(media_file,save_name)
     num = num + 1
+
+client = vision.ImageAnnotatorClient()
+
+for i in range(num):
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'img%03d.jpg'%i)
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    str = ''
+    for label in labels:
+        str = str + label.description + '\n'
+
+    im = Image.open(file_name)
+    draw = ImageDraw.Draw(im)
+    x,y=(100,100)
+    draw.text((x,y), str,)
+    im.save(file_name)
+    print(str)
+
+
 
 os.popen('ffmpeg -r 0.5 -i img%03d.jpg -vf scale=500:500 -y -r 30 -t 60 out.mp4')

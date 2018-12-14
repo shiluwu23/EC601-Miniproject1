339cb403d8e5ff309a89f47971c740cc27f1b3c1
shiluwu23 <43075683+shiluwu23@users.noreply.github.com>
2018-12-14 02:11:37 -0500

    Update mysql3.py

--- mysql3.py
+++ mysql3.py
@@ -7,14 +7,17 @@ import datetime
 
 # Write information into the mini3_database database.
 def mysql(twnum,picnum,picaddress,newtag):
+    # Connect to the database.
     dbb = pymysql.connect(host = "localhost", user = "root", passwd = "", db ="ec601mini3")
     cursor = dbb.cursor()
     times = datetime.datetime.now()
     print(times)
+    # Insert
     sql = "INSERT INTO mini3_database(username, \
            twtnum,picnum,timei, picaddress, tags) \
            VALUES ('%s', %s, %s, '%s', '%s','%s')" % \
           ('Username', twnum, picnum, times, picaddress,newtag)
+
     try:
        cursor.execute(sql)
        dbb.commit()
@@ -23,6 +26,7 @@ def mysql(twnum,picnum,picaddress,newtag):
        # Rollback in case there is any error
        dbb.rollback()
        print("Can't connect to mysql database!")
+
     dbb.close()
 
 # Create table
@@ -49,6 +53,6 @@ def create():
 
 if __name__ == '__main__':
     create()
-    mysql('twnum', 'picnum', 'picaddress', 'newtag')
+    mysql('twtnum', 'picnum', 'picaddress', 'newtag')
 
 
